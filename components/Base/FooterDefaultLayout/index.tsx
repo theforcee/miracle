40a90a28871c1026b53f366b9101c5c8eb54932e
King Wisdom <kingwisdom.dev@gmail.com>
2022-10-21 15:50:27 +0700

    fe: do footer

--- components/Base/FooterDefaultLayout/index.tsx
+++ components/Base/FooterDefaultLayout/index.tsx
@@ -1,17 +1,47 @@
 import clsx from "clsx"
+import Image from "next/image"
+import logoMiracle from "/public/images/logo-full.svg"
+import iconTelegram from "/public/images/social/icon-telegram.svg"
+import iconTwitter from "/public/images/social/icon-twitter.svg"
+import iconLinkedin from "/public/images/social/icon-linkedin.svg"
+import iconFacebook from "/public/images/social/icon-facebook.svg"
+import Link from "next/link"
+
+const socials = [
+  { icon: iconTelegram, name: "Telegram" },
+  { icon: iconTwitter, name: "Twitter" },
+  { icon: iconLinkedin, name: "Linkedin" },
+  { icon: iconFacebook, name: "Facebook" },
+]
 
 const FooterDefaultLayout = () => {
   return (
     <div className="text-white w-full">
       <div
         className={clsx(
-          "flex flex-col relative max-w-screen-main mx-auto px-7 text-center pt-[60px] box-border",
-          "md:px-40 md:pt-24 md:text-left",
+          "flex flex-col relative max-w-[1110px] mx-auto pb-20 text-center pt-[60px] box-border",
+          "md:pt-[120px] md:text-left",
         )}
       >
-        <p className="text-center md:mt-16 mt-3">
-          Copyright © 2022 . All rights reserved.
-        </p>
+        <div className="flex justify-between items-center">
+          <div>
+            <Image src={logoMiracle} alt="" />
+          </div>
+          <div className="flex flex-col items-end">
+            <div className="flex gap-4 items-center">
+              {socials.map((item: any) => (
+                <Link key={item.title} href="">
+                  <div className="relative w-7">
+                    <Image src={item.icon} alt={item.name} />
+                  </div>
+                </Link>
+              ))}
+            </div>
+            <p className="text-center mt-2">
+              © Icetea Labs 2022. All rights reserved
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   )
